25bd5d6cd2084eae82308bfa16995c26e155d648
weilin.hsu <weilin.hsu@ehsn.com.tw>
2021-01-16 15:13:49 +0800

    變更專案名稱

--- CH2 策略模式/StrategyModelExample/StrategyModelExample/CashFactory.cs
+++ CH2 策略模式/StrategyModelExample/StrategyModelExample/CashFactory.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StrategyModelExample
+namespace FactoryModelSolution
 {
     /// <summary>
     /// 現金收費工廠類別
--- CH2 策略模式/StrategyModelExample/StrategyModelExample/CashModule.cs
+++ CH2 策略模式/StrategyModelExample/StrategyModelExample/CashModule.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StrategyModelExample
+namespace FactoryModelSolution
 {
     /// <summary>
     /// 正常收費子類別
