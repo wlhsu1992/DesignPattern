fb80bf509c3f513222be203340f57ba4fdbac5cd
weilin.hsu <weilin.hsu@ehsn.com.tw>
2021-01-16 16:13:13 +0800

    整理程式碼

--- CH2 策略模式/StrategyModelExample/StrategyModelExample/Program.cs
+++ CH2 策略模式/StrategyModelExample/StrategyModelExample/Program.cs
@@ -12,23 +12,43 @@ namespace FactoryModelSolution
         {
             string[] cashModel = new string[] { "正常收費", "滿300送100", "打8折" };
 
+            //工廠模式生成計價類別物件
+            CashSuper csuper = CashFactory.createCashAccept(getCashModel(cashModel));
+
+            //取得商品價格、數量資訊
+            double price = getProductPrice();
+            int num = getProductNumber();
+
+            //使用多型計算應收金額
+            double totalPrice = csuper.acceptCash(price * num);
+            printCashInfo(price, num, totalPrice);
+        }
+
+        static string getCashModel(string[] cashModel)
+        {
             Console.WriteLine("請選擇計價模式");
             for (int i = 0; i < cashModel.Length; i++)
-                Console.WriteLine($"{i+1}. {cashModel[i]}");
+                Console.WriteLine($"{i + 1}. {cashModel[i]}");
             int selectModelIndex = Convert.ToInt32(Console.ReadLine()) - 1;
-            Console.WriteLine($"選擇計價模式: {cashModel[selectModelIndex]}");
-            Console.WriteLine();
-            //工廠模式生成計價類別物件
-            CashSuper csuper = CashFactory.createCashAccept(cashModel[selectModelIndex]);
+            string selectCashModel = cashModel[selectModelIndex];
+            Console.WriteLine($"選擇計價模式: {selectCashModel}");
+            return selectCashModel;
+        }
 
+        static double getProductPrice()
+        {
             Console.WriteLine("輸入商品單價:");
-            int price = Convert.ToInt32(Console.ReadLine());
+            return Convert.ToDouble(Console.ReadLine());
+        }
 
+        static int getProductNumber()
+        {
             Console.WriteLine("輸入商品件數:");
-            int num = Convert.ToInt32(Console.ReadLine());
+            return Convert.ToInt32(Console.ReadLine());
+        }
 
-            //多型計算應收金額
-            double totalPrice = csuper.acceptCash(price * num);
+        static void printCashInfo(double price, int num, double totalPrice)
+        {
             Console.WriteLine($"單價: {price}\n數量: {num}\n總額: {totalPrice}");
             Console.ReadLine();
         }
