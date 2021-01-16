977dee52673c4e050af9c25a46a06c5af6efab14
weilin.hsu <weilin.hsu@ehsn.com.tw>
2021-01-16 23:37:21 +0800

    商場收銀軟體 v2

---
+++ CH2 策略模式/StrategyModelExample/1_CashSystem/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_CashSystem
+{
+    public class Product
+    {
+        public double Price { get; }
+        public int Num { get; }
+
+        public Product(double price, int num)
+        {
+            Price = price;
+            Num = num;
+        }
+    }
+}
--- CH2 策略模式/StrategyModelExample/1_CashSystem/Program.cs
+++ CH2 策略模式/StrategyModelExample/1_CashSystem/Program.cs
@@ -10,6 +10,50 @@ namespace _1_CashSystem
     {
         static void Main(string[] args)
         {
+            //初始化結帳商品單價及數量
+            Product product = new Product(1000, 1);
+
+            //計算商品結帳總額
+            double discount = getDiscount();
+            double totalPrice = product.Price * product.Num * discount;
+
+            //印出結帳資訊
+            printCashInfo(product.Price, product.Num, totalPrice);
+        }
+
+        /// <summary>
+        /// 取得商品折數
+        /// </summary>
+        static double getDiscount()
+        {
+            string[] cashModel = new string[] { "正常收費", "打八折", "打七折", "打五折" };
+            Console.WriteLine("請選擇計價模式");
+            for (int i = 0; i < cashModel.Length; i++)
+                Console.WriteLine($"{i + 1}. {cashModel[i]}");
+            int selectModelIndex = Convert.ToInt32(Console.ReadLine()) - 1;
+
+            switch (selectModelIndex) 
+            {
+                case 0:
+                    return 1.0;
+                case 1:
+                    return 0.8;
+                case 2:
+                    return 0.7;
+                case 3:
+                    return 0.5;
+                default:
+                    throw new Exception("輸入錯誤");
+            }
+        }
+
+        /// <summary>
+        /// 印出結帳資訊
+        /// </summary>
+        static void printCashInfo(double price, int num, double totalPrice)
+        {
+            Console.WriteLine($"單價: {price}\n數量: {num}\n總額: {totalPrice}");
+            Console.ReadLine();
         }
     }
 }
